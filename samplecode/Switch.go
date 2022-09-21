afe14d05dd5614f6f1146d5436c1bda2cb0a0342
LYWdev <yoonwoodev@kakao.com>
2022-09-21 15:15:22 +0900

    te

--- samplecode/Switch.go
+++ samplecode/Switch.go
@@ -20,7 +20,6 @@ func main () {
 	switch time.Now().Weekday(){
 	case time.Saturday, time.Sunday:
 		fmt.Println("It's the weekend")
-	
 	default:
 		fmt.Println("It's a weekday")
 	}
@@ -32,6 +31,7 @@ func main () {
 	default:
 		fmt.Println("It's after noon")
 	}
+
 	whatAmI := func(i interface{}){
 		switch t:=i.(type){
 		case bool:
@@ -42,7 +42,9 @@ func main () {
 			fmt.Println("Don't Know type %T\n",t)
 		}
 	}
-	whatAmI("Hello\n",true)
+
+	whatAmI(interface{"Hello\n",true})
 	whatAmI(1)
 	whatAmI("hey")
+
 }
