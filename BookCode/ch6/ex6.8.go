7f1aa46b920c7b914087c76d4caddf37f70cf18d
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2022-12-28 16:30:40 +0900

    스왑 및 연산자까지 끝

--- BookCode/ch6/ex6.8.go
+++ BookCode/ch6/ex6.8.go
@@ -12,6 +12,7 @@ func main() {
 	var a float64 = 0.1
 	var b float64 = 0.2
 	var c float64 = 0.3
+	x,y := 1,2
 
 	fmt.Printf("%0.18f + %0.18f = %0.18f\n", a, b, a+b)
 	fmt.Printf("%0.18f == %0.18f : %v\n", c, a+b,equal(a+b,c))
@@ -21,6 +22,11 @@ func main() {
     c = 0.00000000007
 
 	fmt.Printf("%g == %g : %v\n", c, a+b, equal(a+b,c))
+
+	//swap
+	fmt.Printf(" BEFORE SWAP  x: %d, y: %d\n", x, y)
+	x,y = y,x
+	fmt.Printf(" AFTER SWAP  x: %d, y: %d\n", x, y)
 
 
 }
