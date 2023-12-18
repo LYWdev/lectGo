e71378daf9d485d5a09b88c6b979e18be65d97f2
LYWdev <yoonwoodev@kakao.com>
2023-12-18 12:05:08 +0900

    complete cli

--- project/client.go
+++ project/client.go
@@ -1,31 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "net"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-    // 서버에 연결
-    conn, err := net.Dial("tcp", "localhost:8080")
-    if err != nil {
-        fmt.Println("Error connecting:", err.Error())
-        return
-    }
-    defer conn.Close()
+	// 서버 주소와 포트 설정
+	serverAddress := "192.168.64.16"
+	serverPort := "8080"
 
-    // 서버로 메시지 전송
-    message := "Hello, server!"
-    conn.Write([]byte(message))
+	// 서버에 연결
+	conn, err := net.Dial("tcp", serverAddress+":"+serverPort)
+	if err != nil {
+		log.Fatal("Error connecting to server:", err)
+	}
+	defer conn.Close()
 
-    // 서버로부터 응답 수신
-    buffer := make([]byte, 1024)
-    _, err = conn.Read(buffer)
-    if err != nil {
-        fmt.Println("Error reading:", err.Error())
-        return
-    }
+	log.Println("Connected to server. You can start chatting.")
 
-    // 서버 응답 출력
-    fmt.Println("Server response:", string(buffer))
+	go receiveMessages(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		message := scanner.Text()
+		_, err := conn.Write([]byte(message + "\n"))
+		if err != nil {
+			log.Println("Error sending message:", err)
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println("Error reading input:", err)
+	}
 }
+
+// 서버로부터 메시지 수신 함수
+func receiveMessages(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error receiving message:", err)
+			break
+		}
+
+		message = message[:len(message)-1]
+		fmt.Println("Received message:", message)
+	}
+
+	log.Println("Disconnected from server.")
+}
+
